f920c047d8be856386c06e035488521fc87abf0d
ehime <dodomeki@gmail.com>
2014-10-08 16:34:22 -0700

    Adding initial installer scrip

---
+++ shell/installer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+
+## ROOT check
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as su" 1>&2 ; exit 1
+fi
+
+cd /tmp
+wget 'https://storage.googleapis.com/golang/go1.3.3.darwin-amd64-osx10.8.pkg'
+installer -pkg 'go1.3.3.darwin-amd64-osx10.8.pkg' -target /
