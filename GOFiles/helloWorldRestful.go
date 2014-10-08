94c65e4cb3fc39f83c40f211fc0c7474a04743f9
ehime <dodomeki@gmail.com>
2014-10-08 16:57:31 -0700

    Pushing comments

--- GOFiles/helloWorldRestful.go
+++ GOFiles/helloWorldRestful.go
@@ -10,8 +10,12 @@ func main() {
 	http.ListenAndServe("localhost:8000", nil)
 }
 
+/**
+ * w is an instantiation of http.ResponseWriter
+ * r is an instantiation of http.Response (read)
+ */
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, GOLang")
 }
 
-// curl -X GET localhost:8000 -> Hello, GOLang
+//RUNBOX: curl -X GET localhost:8000 -> Hello, GOLang
