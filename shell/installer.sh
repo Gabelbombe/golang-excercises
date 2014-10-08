213dec32f4cf163b51b70f837d3cf0059d69d409
ehime <dodomeki@gmail.com>
2014-10-08 16:58:06 -0700

    Updating for osx launchctl

--- shell/installer.sh
+++ shell/installer.sh
@@ -9,3 +9,6 @@ fi
 cd /tmp
 wget 'https://storage.googleapis.com/golang/go1.3.3.darwin-amd64-osx10.8.pkg'
 installer -pkg 'go1.3.3.darwin-amd64-osx10.8.pkg' -target /
+
+launchctl setenv GOROOT `which go`
+launchctl setenv GOPATH /Users/`whoami`/go
