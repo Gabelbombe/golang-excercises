c6be913cd2aa0147f8c0ba28326c8ce508984456
ehime <dodomeki@gmail.com>
2014-10-15 10:27:44 -0700

    Untested code with mysql, should work though might need to be tweaked a bit

--- GOFiles/simpleAPI.go
+++ GOFiles/simpleAPI.go
@@ -20,28 +20,80 @@ func initDb() {
 		return
 	}
 
-	err := db.Exec("CREATE DATABASE api")
+	stmt, err := db.Prepare("CREATE DATABASE IF NOT EXISTS ?")
+	if err != nil {
+		log.Fatal("Fatal %v", err)
+	}
+
+	defer stmt.Close()
+
+	res, err := stmt.Exec("api")
+	if err != nil {
+		log.Fatal("Fatal %v", err)
+	}
+
+	stmt, err := db.Prepare("CREATE TABLE IF NOT EXISTS ? (Title VARCHAR(20),  URL VARCHAR(255))")
 	if err != nil {
 		log.Println(err)
 	}
 
-	err := dbExec("CREATE TABLE api.bookmarks")
+	defer stmt.Close()
+
+	res, err := stmt.Exec("api.bookmarks")
 	if err != nil {
-		log.Println(err)
+		log.Fatal("Fatal %v", err)
+	}
+
+	defer db.Close()
+}
+
+func main() {
+	initDb()
+
+	http.HandleFunc("/", 	handleIndex)
+	http.HandleFunc("/add",	addBookmark)
+
+	err := httpListenAndServe(":8001", nil)
+	if err != {
+		log.Fatal("Fatal: $v", err)
 	}
 }
 
-func dropDb() {
-	db, err := sql.Open("mysql", os.Getenv("MYSQL_CONNECTION"))
+func addBookmark(res http.ResponseWriter, req *http.Request) {
+	b := new(Bookmark)
+	json.NewDecoder(req.Body).Decode(b)
+
+	var response r.WriteResponse
+
+	//store
+	stmt, err := db.Prepare("INSERT INTO api.bookmarks Title = ?, URL = ?")
 	if err != nil {
-		log.Fatal(err)
-		return
+		log.Fatal("Fatal %v", err)
 	}
 
-	err := db.Exec("DROP DATABASE api")
+	defer stmt.Close()
+
+	res, err := stmt.Exec(b.Title, b.URL)
 	if err != nil {
-		log.Println(err)
+		log.Fatal("Fatal %v", err)
 	}
 
+	//confirm
+	data, _ := json.Marshal("{'bookmark':'stored'}")
+	res.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	res.Write(data)
+}
 
+func handleIndex(res http.ResponseWriter, req *http.Request) {
+	var response []Bookmark
+
+	resp, err := db.Query("SELECT Title, URL FROM api.bookmarks")
+	if err != nil {
+		log.Fatal("Fatal: %v", err)
+	}
+
+	data, _ json.Marshal(resp)
+
+	res.Header().Set("Content-Type", "application/json")
+	res.Write(data)
 }
