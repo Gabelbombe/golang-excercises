3bde7eba6d006b25264d6d74c87c47ea832de18b
ehime <dodomeki@gmail.com>
2014-10-30 16:44:12 -0700

    Adding editor

--- GOFiles/wiki.go
+++ GOFiles/wiki.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"io/ioutil"
 	"net/http"
+	"fmt"
 )
 
 // data struct
@@ -20,11 +21,10 @@ func (p *Page) save() error {
 func loadPage (title string) (*Page, error) {
 	filename  := title + ".txt"
 	body, _ := ioutil.ReadFile(filename)			// _ is a blank identifier, essentially assigning error to /dev/null
-
 	return &Page {Title: title, Body: body}, nil
 }
 
-func genericHandler (w http.ResponseWriter, r *http.Request) {
+func oopsHandler (w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "I love %s...\n", r.URL.Path[1:])
 }
 
@@ -34,8 +34,23 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title  := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	t, _ := template.ParseFiles("/pages/edit.html")
+	t.Execute(w, p)
+}
+
+
 func main() {
-	http.HandleFunc("/", 	  genericHandler)
+	http.HandleFunc("/", 	  oopsHandler)
+
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+
 	http.ListenAndServe(":8080", nil)
 }
