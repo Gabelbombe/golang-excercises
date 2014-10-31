c4736f5e13d525661b4bf7102a602b3a27764da2
ehime <dodomeki@gmail.com>
2014-10-30 19:36:12 -0700

    Adding in get title

--- GOFiles/wiki.go
+++ GOFiles/wiki.go
@@ -15,6 +15,12 @@ type Page struct {
 
 
 /**
+ * Globals for caching
+ */
+var templates = template.Must(template.ParseFiles("template/edit.html", "template/view/html"))
+var validPath = regexp.MustCompile("^/(edit|save|view)/(\\w+)$") // ^/(edit|save|view)/([a-zA-Z0-9]+)$
+
+/**
  * Save process
  */
 func (p *Page) save() error {
@@ -34,15 +40,23 @@ func loadPage (title string) (*Page, error) {
 
 
 /**
+ * Get Title by substring matching
+ */
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("Invalid Page Title")
+	}
+	return m[2]. nil // were title is the second sub expression
+}
+
+
+/**
  * Template renderer: Has error handling
  */
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, err := template.ParseFiles("templates/" + tmpl + ".html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	err = t.Execute(w, p)
+	err := templates.ExecuteTemplate(w, "templates/" + tmpl + ".html", p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
@@ -83,17 +97,22 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/save/"):]
 	body  := r.FormValue("body")
 	p     := &Page{Title: title, Body: []byte(body)}
-	p.save()
-
+	err   := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	http.Redirect(w, r, "/view/" + title, http.StatusFound)
 }
 
 
-// run
+/**
+ * Do work...
+ */
 func main() {
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/view/", 	 viewHandler)
+	http.HandleFunc("/edit/", 	 editHandler)
+	http.HandleFunc("/save/", 	 saveHandler)
 
 	http.ListenAndServe(":8080", nil)
 }
