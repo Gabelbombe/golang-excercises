80f3aeb738cee2a709998f9193cf5b2ffe715dc7
ehime <dodomeki@gmail.com>
2014-10-30 19:11:30 -0700

    comments

--- GOFiles/wiki.go
+++ GOFiles/wiki.go
@@ -4,7 +4,7 @@ import (
 	"html/template"
 	"io/ioutil"
 	"net/http"
-	"fmt"
+//	"fmt"
 )
 
 // data struct
@@ -14,22 +14,37 @@ type Page struct {
 }													// REF: http://blog.golang.org/go-slices-usage-and-internals
 
 
+/**
+ * Save process
+ */
 func (p *Page) save() error {
 	filename := "templates/" +  p.Title + ".html"
 	return ioutil.WriteFile(filename, p.Body, 0600) // 0600 permissions
 }
 
+
+/**
+ * Page Loading
+ */
 func loadPage (title string) (*Page, error) {
 	filename  := title + ".txt"
 	body, _ := ioutil.ReadFile(filename)
 	return &Page {Title: title, Body: body}, nil
 }
 
+
+/**
+ * Template renderer
+ */
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	t, _ := template.ParseFiles("templates/" + tmpl + ".html")
 	t.Execute(w, p)
 }
 
+
+/**
+ * View Handler
+ */
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title  := r.URL.Path[len("/view/"):]
 	p, err := loadPage(title)
@@ -40,6 +55,10 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "view", p)
 }
 
+
+/**
+ * Edit Handler
+ */
 func editHandler(w http.ResponseWriter, r *http.Request) {
 	title  := r.URL.Path[len("/edit/"):]
 	p, err := loadPage(title)
@@ -49,6 +68,10 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "edit", p)
 }
 
+
+/**
+ * Save Handler
+ */
 func saveHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/save/"):]
 	body  := r.FormValue("body")
@@ -58,9 +81,12 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/view/" + title, http.StatusFound)
 }
 
+
+// run
 func main() {
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
 	http.HandleFunc("/save/", saveHandler)
+
 	http.ListenAndServe(":8080", nil)
 }
